77e14f21ca1def2ea32b4b811129691c1e3c5a08
Nathan Santiago <santiagonathan9254@gmail.com>
2026-02-12 13:23:06 -0500

    Install biome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,33 @@
-import './App.css'
-import { Circle, Layer, Stage } from "react-konva"
-import Grid from './components/Grid'
+import './App.css';
+import { Circle, Layer, Stage } from 'react-konva';
+import Grid from './components/Grid';
 
 function App() {
-  var width = window.innerWidth
-  var height = window.innerHeight
-  var gridWidth = 50
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var gridWidth = 50;
 
   return (
     <Stage width={width} height={height}>
       <Layer>
         <Grid width={width} height={height} gridWidth={gridWidth} />
-        <Circle width={50} height={50} x={100} y={100} fill="red" draggable
+        <Circle
+          width={50}
+          height={50}
+          x={100}
+          y={100}
+          fill="red"
+          draggable
           onMouseEnter={(e) => {
             document.body.style.cursor = 'pointer';
           }}
           onMouseLeave={(e) => {
             document.body.style.cursor = 'default';
-          }} />
+          }}
+        />
       </Layer>
     </Stage>
-  )
+  );
 }
 
-export default App
+export default App;
